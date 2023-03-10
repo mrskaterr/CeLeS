8150bdd1d9e82e6656ae58f6b99a63645c4629ca
paw <pwawrzyniak@edu.cdv.pl>
2023-03-10 17:57:08 +0100

    taski

--- Assets/Scripts/TASKS/taskCircle/Hack_Panel.cs
+++ Assets/Scripts/TASKS/taskCircle/Hack_Panel.cs
@@ -11,22 +11,39 @@ public class Hack_Panel : MonoBehaviour
     [SerializeField] Image[] images;
     int lRot, rRot;
     int lRand, rRand;
+
+
+    float XScal, YScal;
+
+
     [SerializeField] Color green;
     [SerializeField] Color blue;
+    [SerializeField] RectTransform sinRand;
+    [SerializeField] RectTransform sin;
     int c = 0;
     bool stop;
 
     private void Start()
     {
         Rand();
+        sinRand.localScale=new Vector3(XScal, YScal, 1);
     }
 
     private void FixedUpdate()
     {
         if (c <= 3 && !stop)
         {
-            if (Input.GetKey(KeyCode.A)) { left.Rotate(Vector3.back * 1.5f); }
-            else if (Input.GetKey(KeyCode.D)) { left.Rotate(Vector3.forward * 1.5f); }
+            if (Input.GetKey(KeyCode.A))
+            {
+                left.Rotate(Vector3.back * 1.5f);
+            }
+
+
+            else if (Input.GetKey(KeyCode.D))
+            {
+                left.Rotate(Vector3.forward * 1.5f);
+            }
+            sin.localScale = new Vector3((lRand)/359f, sin.localScale.y, 1f);
 
             if (Input.GetKey(KeyCode.LeftArrow)) { right.Rotate(Vector3.back * 1.5f); }
             else if (Input.GetKey(KeyCode.RightArrow)) { right.Rotate(Vector3.forward * 1.5f); }
@@ -57,9 +74,14 @@ public class Hack_Panel : MonoBehaviour
 
     void Rand()
     {
-        lRand = Random.Range(0, 360);
-        rRand = Random.Range(0, 360);
+        XScal = Random.Range(0.5f, 6f);
+        YScal = Random.Range(0.10f,1f);
+        Debug.Log((int)(XScal / 6f * 359));
+        Debug.Log((int)(YScal / 6f * 359));
+        lRand = (int)(XScal / 6f * 359);
+        rRand = (int)(YScal / 1f * 359);
     }
+
 
     bool CheckLeft()
     {

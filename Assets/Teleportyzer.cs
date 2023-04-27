4fd135c061ecfbd8845593815d3ab4d60ccf99a1
Pawel <mrskatervv@gmail.com>
2023-04-27 10:42:25 +0200

    wal sie na cyce dawid

---
+++ Assets/Teleportyzer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportyzer : MonoBehaviour
+{
+    [SerializeField] Transform TransformToTeleport;
+
+    public void Interact(GameObject @object)
+    {
+        
+    }
+}

2605dd0d4a079476dd53857383988d6955be0f1a
Dave K <Dave04711@gmail.com>
2023-06-22 15:05:57 +0200

    Compass Final

--- Assets/Scripts/CompassHandler.cs
+++ Assets/Scripts/CompassHandler.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class CompassHandler : MonoBehaviour
 {
-    [SerializeField] private Transform compassObject;
-    [SerializeField] private List<MeshRenderer> renderers;
+    [SerializeField] private Material compassMat;
+    [SerializeField] private float offset = .52f;
     [SerializeField] private float turnSpeed = .1f;
     [Space]
     [SerializeField] private AnimationCurve fadeCurve;
+    [Space]
+    [SerializeField] private RectTransform ping;
+    [SerializeField] private Camera cam;
 
     private Quaternion goal;
 
@@ -19,61 +22,15 @@ public class CompassHandler : MonoBehaviour
 
     private void Update()
     {
-        compassObject.rotation = Quaternion.Slerp(compassObject.rotation, goal, turnSpeed);
-
         float parentRot = transform.eulerAngles.y;
-
-        //int centerIndex = FindCenter(parentRot);
-
-        #region TODO: LOOP
-        SetOpacity(renderers[0], CalculateOpacity(parentRot, 0));//N - 0
-        SetOpacity(renderers[1], CalculateOpacity(parentRot, 15));//15
-        SetOpacity(renderers[2], CalculateOpacity(parentRot, 30));//30
-        SetOpacity(renderers[3], CalculateOpacity(parentRot, 45));//45
-        SetOpacity(renderers[4], CalculateOpacity(parentRot, 60));//60
-        SetOpacity(renderers[5], CalculateOpacity(parentRot, 75));//75
-        SetOpacity(renderers[6], CalculateOpacity(parentRot, 90));//E - 90
-        SetOpacity(renderers[7], CalculateOpacity(parentRot, 105));//105
-        SetOpacity(renderers[8], CalculateOpacity(parentRot, 120));//120
-        SetOpacity(renderers[9], CalculateOpacity(parentRot, 135));//135
-        SetOpacity(renderers[10], CalculateOpacity(parentRot, 150));//150
-        SetOpacity(renderers[11], CalculateOpacity(parentRot, 165));//165
-        SetOpacity(renderers[12], CalculateOpacity(parentRot, 180));//S - 180
-        SetOpacity(renderers[13], CalculateOpacity(parentRot, 195));//195
-        SetOpacity(renderers[14], CalculateOpacity(parentRot, 210));//210
-        SetOpacity(renderers[15], CalculateOpacity(parentRot, 225));//225
-        SetOpacity(renderers[16], CalculateOpacity(parentRot, 240));//240
-        SetOpacity(renderers[17], CalculateOpacity(parentRot, 255));//255
-        SetOpacity(renderers[18], CalculateOpacity(parentRot, 270));//W - 270
-        SetOpacity(renderers[19], CalculateOpacity(parentRot, 285));//285
-        SetOpacity(renderers[20], CalculateOpacity(parentRot, 300));//300
-        SetOpacity(renderers[21], CalculateOpacity(parentRot, 315));//315
-        SetOpacity(renderers[22], CalculateOpacity(parentRot, 330));//330
-        SetOpacity(renderers[23], CalculateOpacity(parentRot, 345));//345 
-        #endregion
+        compassMat.SetFloat("_Rotation", Mathf.Clamp01(parentRot / 360f) + offset);
+        SetPingPos();
     }
 
-    //private int FindCenter(float _rot)
-    //{
-    //    float rawIndex = _rot / 15f;
-    //    int index = Mathf.RoundToInt(rawIndex);
-    //    if (index == 24) { index= 0; }
-    //    return index;
-    //}
-
-    private float CalculateOpacity(float _degrees, int _target)
+    private void SetPingPos()
     {
-
-        //float value = Mathf.Abs(_degrees - _target);
-        Quaternion A = Quaternion.Euler(0, _degrees, 0);
-        Quaternion B = Quaternion.Euler(0, _target, 0);
-        float angle = Quaternion.Angle(A, B);
-        //return 1 - Mathf.Clamp01(value / 180f);
-        return fadeCurve.Evaluate(Mathf.Clamp01(angle / 360f));
-    }
-
-    private void SetOpacity(MeshRenderer _renderer, float _value)
-    {
-        _renderer.material.color = new Color(1, 1, 1, _value);
+        Vector3 viewPos = cam.WorldToViewportPoint(Vector3.zero);
+        float X = viewPos.x * 1542.024f;
+        ping.anchoredPosition = new Vector2(X, 0);
     }
 }
